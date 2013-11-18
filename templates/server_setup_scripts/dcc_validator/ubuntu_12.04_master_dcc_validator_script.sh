acd225cb50da63d55c67424076e01192cf6af657
Brian O'Connor <briandoconnor@gmail.com>
2013-11-18 15:21:21 -0500

    Changes for the DCC validation system, bringing this up to release 2.1.0 from 2.0.1.

--- templates/server_setup_scripts/dcc_validator/ubuntu_12.04_master_dcc_validator_script.sh
+++ templates/server_setup_scripts/dcc_validator/ubuntu_12.04_master_dcc_validator_script.sh
@@ -5,6 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 # prepare a location for the DCC validator
 mkdir -p /mnt/dcc-portal
+mkdir -p /tmp/icgc
 cd /mnt/dcc-portal
 
 # setup Mongo
@@ -19,17 +20,26 @@ service mongodb restart
 sleep 5m
 
 # now get the DCC app
-wget http://seqwaremaven.oicr.on.ca/artifactory/dcc-release/org/icgc/dcc/dcc-submission-server/2.0.1/dcc-submission-server-2.0.1-dist.tar.gz
-tar zxf dcc-submission-server-2.0.1-dist.tar.gz
+wget http://seqwaremaven.oicr.on.ca/artifactory/dcc-release/org/icgc/dcc/dcc-submission-server/2.1.0/dcc-submission-server-2.1.0-dist.tar.gz
+tar zxf dcc-submission-server-2.1.0-dist.tar.gz
 
 # copy the correct config that has been customized for DCC
-cp /vagrant/application.conf /mnt/dcc-portal/dcc-submission-server-2.0.1/conf/
-cp /vagrant/init.sh /mnt/dcc-portal/dcc-submission-server-2.0.1/
+cp /vagrant/application.conf /mnt/dcc-portal/dcc-submission-server-2.1.0/conf/
+cp /vagrant/init.sh /mnt/dcc-portal/dcc-submission-server-2.1.0/
+
+# get the reference genome
+# FIXME: I just drop this off in /tmp?!?!
+wget 
+wget 
+gunzip GRCh37.fasta.gz
+ln -s /mnt/dcc-portal/GRCh37.fasta.fai /tmp/
+ln -s /mnt/dcc-portal/GRCh37.fasta /tmp/
 
 # start the process
-/bin/sh /mnt/dcc-portal/dcc-submission-server-2.0.1/bin/dcc-submission-server install
+/mnt/dcc-portal/dcc-submission-server-2.1.0/bin/install -s
+/bin/sh /mnt/dcc-portal/dcc-submission-server-2.1.0/bin/dcc-submission-server install -s
 service dcc-submission-server start
 
 # run the init process
-bash /mnt/dcc-portal/dcc-submission-server-2.0.1/init.sh http://%{DCC_VALIDATOR_DICTIONARY_SERVER}:%{DCC_VALIDATOR_DICTIONARY_PORT} http://localhost:5380 %{DCC_VALIDATOR_USER} %{DCC_VALIDATOR_PASSWD} Release1 project1 Project1 Project1
+bash /mnt/dcc-portal/dcc-submission-server-2.1.0/init.sh http://%{DCC_VALIDATOR_DICTIONARY_SERVER}:%{DCC_VALIDATOR_DICTIONARY_PORT} http://localhost:5380 %{DCC_VALIDATOR_USER} %{DCC_VALIDATOR_PASSWD} Release1 project1 Project1 Project1
 
