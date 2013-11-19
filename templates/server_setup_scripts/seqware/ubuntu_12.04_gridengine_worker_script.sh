5ecdf55a11ff01990f23d036d74ee0046de63b89
Denis Yuen <denis.yuen@oicr.on.ca>
2013-11-19 17:06:41 -0500

    A work in progress, worker script, and an updated template

--- templates/server_setup_scripts/seqware/ubuntu_12.04_gridengine_worker_script.sh
+++ templates/server_setup_scripts/seqware/ubuntu_12.04_gridengine_worker_script.sh
@@ -6,40 +6,32 @@
 # watch qstat -f"
 # you should then see your job run and also see STDIN.* files get created with the hostname in them
 
+# first, fix the /etc/hosts file since SGE wants reverse lookup to work
+cp /etc/hosts /tmp/hosts
+echo `/sbin/ifconfig  | grep -A 3 eth0 | grep 'inet addr' | perl -e 'while(<>){ chomp; /inet addr:(\d+\.\d+\.\d+\.\d+)/; print $1; }'` `hostname` > /etc/hosts
+cat /tmp/hosts | grep -v '127.0.1.1' >> /etc/hosts
+
+# setup hosts
+# NOTE: the hostname seems to already be set at least on BioNimubs OS
+echo '%{HOSTS}' >> /etc/hosts
+hostname  $HOST
+
 # get packages
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
-apt-get -q -y --force-yes install gridengine-client gridengine-common gridengine-exec gridengine-master
+apt-get -q -y --force-yes install gridengine-client gridengine-common gridengine-exec 
 
 # restart
 /etc/init.d/gridengine-exec stop
-/etc/init.d/gridengine-master restart
 /etc/init.d/gridengine-exec start
 
 # configure
 export HOST=`hostname`
 sudo -u sgeadmin qconf -am seqware
 qconf -au seqware users
-qconf -as $HOST
 
-# this is interactive... how do I load from a file?
-cat >/tmp/qconf-editor.sh <<EOF
-#!/bin/sh
-sleep 1
-perl -pi -e 's/^hostname.*$/hostname $HOST/' \$1
-EOF
-chmod +x /tmp/qconf-editor.sh
-export EDITOR=/tmp/qconf-editor.sh
-qconf -ae
+qconf -ah $HOST 
 
-# now do this again
-cat >/tmp/qconf-editor.sh <<EOF
-#!/bin/sh
-sleep 1
-perl -pi -e 's/^hostlist.*$/hostlist $HOST/' \$1
-EOF
-chmod +x /tmp/qconf-editor.sh
-export EDITOR=/tmp/qconf-editor.sh
 qconf -ahgrp @allhosts
 # might need to do this instead
 qconf -mhgrp @allhosts
@@ -59,8 +51,5 @@ qconf -aattr queue slots "[$HOST=1]" main.q
 
 # restart
 /etc/init.d/gridengine-exec stop
-/etc/init.d/gridengine-master restart
 /etc/init.d/gridengine-exec start
 
-# change seqware engine to oozie-sge
-perl -pi -e 's/SW_DEFAULT_WORKFLOW_ENGINE=oozie/SW_DEFAULT_WORKFLOW_ENGINE=oozie-sge/' /home/seqware/.seqware/settings
