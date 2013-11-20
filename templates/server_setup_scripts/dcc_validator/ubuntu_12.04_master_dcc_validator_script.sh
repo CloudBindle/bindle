bdaf33087db5c9e0715f5fa200e48583586a4d8a
Brian O'Connor <briandoconnor@gmail.com>
2013-11-19 22:17:03 -0500

    Trying 2.0.1, will revert to 2.1.0

--- templates/server_setup_scripts/dcc_validator/ubuntu_12.04_master_dcc_validator_script.sh
+++ templates/server_setup_scripts/dcc_validator/ubuntu_12.04_master_dcc_validator_script.sh
@@ -21,12 +21,16 @@ service mongodb restart
 sleep 5m
 
 # now get the DCC app
-wget http://seqwaremaven.oicr.on.ca/artifactory/dcc-release/org/icgc/dcc/dcc-submission-server/2.1.0/dcc-submission-server-2.1.0-dist.tar.gz
-tar zxf dcc-submission-server-2.1.0-dist.tar.gz
+##wget http://seqwaremaven.oicr.on.ca/artifactory/dcc-release/org/icgc/dcc/dcc-submission-server/2.1.0/dcc-submission-server-2.1.0-dist.tar.gz
+##tar zxf dcc-submission-server-2.1.0-dist.tar.gz
+wget http://seqwaremaven.oicr.on.ca/artifactory/dcc-release/org/icgc/dcc/dcc-submission-server/2.0.1/dcc-submission-server-2.0.1-dist.tar.gz
+tar zxf dcc-submission-server-2.0.1-dist.tar.gz
 
 # copy the correct config that has been customized for DCC
-cp /vagrant/application.conf /mnt/dcc-portal/dcc-submission-server-2.1.0/conf/
-cp /vagrant/init.sh /mnt/dcc-portal/dcc-submission-server-2.1.0/
+##cp /vagrant/application.conf /mnt/dcc-portal/dcc-submission-server-2.1.0/conf/
+##cp /vagrant/init.sh /mnt/dcc-portal/dcc-submission-server-2.1.0/
+cp /vagrant/application.conf /mnt/dcc-portal/dcc-submission-server-2.0.1/conf/
+cp /vagrant/init.sh /mnt/dcc-portal/dcc-submission-server-2.0.1/
 
 # get the reference genome
 # FIXME: I just drop this off in /tmp?!?!
@@ -37,10 +41,15 @@ rm dcc-reference-genome-GRCh37.tar.gz
 cd ..
 
 # start the process
-/mnt/dcc-portal/dcc-submission-server-2.1.0/bin/install -s
-/bin/sh /mnt/dcc-portal/dcc-submission-server-2.1.0/bin/dcc-submission-server install -s
+# FIXME: are these redundant!?!?
+##/mnt/dcc-portal/dcc-submission-server-2.1.0/bin/install -s
+##/bin/sh /mnt/dcc-portal/dcc-submission-server-2.1.0/bin/dcc-submission-server install -s
+/mnt/dcc-portal/dcc-submission-server-2.0.1/bin/install -s
+/bin/sh /mnt/dcc-portal/dcc-submission-server-2.0.1/bin/dcc-submission-server install -s
+
 service dcc-submission-server start
 
 # run the init process
-bash /mnt/dcc-portal/dcc-submission-server-2.1.0/init.sh http://%{DCC_VALIDATOR_DICTIONARY_SERVER}:%{DCC_VALIDATOR_DICTIONARY_PORT} http://localhost:5380 %{DCC_VALIDATOR_USER} %{DCC_VALIDATOR_PASSWD} Release1 project1 Project1 Project1
+##bash /mnt/dcc-portal/dcc-submission-server-2.1.0/init.sh http://%{DCC_VALIDATOR_DICTIONARY_SERVER}:%{DCC_VALIDATOR_DICTIONARY_PORT} http://localhost:5380 %{DCC_VALIDATOR_USER} %{DCC_VALIDATOR_PASSWD} Release1 project1 Project1 Project1
+bash /mnt/dcc-portal/dcc-submission-server-2.0.1/init.sh http://%{DCC_VALIDATOR_DICTIONARY_SERVER}:%{DCC_VALIDATOR_DICTIONARY_PORT} http://localhost:5380 %{DCC_VALIDATOR_USER} %{DCC_VALIDATOR_PASSWD} Release1 project1 Project1 Project1
 
