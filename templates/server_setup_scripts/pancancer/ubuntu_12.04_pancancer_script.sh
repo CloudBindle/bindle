4df18bdd3ee0e12c14235bf04d7402dce640419f
Brian O'Connor <briandoconnor@gmail.com>
2014-01-30 10:11:48 +0100

    Random stuff for GNOS prototyping, not working yet

--- templates/server_setup_scripts/pancancer/ubuntu_12.04_pancancer_script.sh
+++ templates/server_setup_scripts/pancancer/ubuntu_12.04_pancancer_script.sh
@@ -1,3 +1,14 @@
 # a place for PanCancer specific config
 apt-get install liblz-dev zlib1g-dev libxml-dom-perl samtools
+# see below for newer versions
+apt-get install genetorrent-common genetorrent-upload genetorrent-download
+# public key
+wget https://cghub.ucsc.edu/software/downloads/cghub_public.key
+
+#genetorrent-download_3.8.5-ubuntu2-12.04_amd64.deb
+#dpkg -i genetorrent-upload_3.8.5-ubuntu2-12.04_amd64.deb  genetorrent-common_3.8.5-ubuntu2-12.04_amd64.deb
+#wget --user=<FILLMEIN> --password=<FILLMEIN> http://annai-repo.annailabs.com/ubuntu-12.04/genetorrent-upload_3.8.5-ubuntu2-12.04_amd64.deb
+#wget --user=<FILLMEIN> --password=<FILLMEIN> http://annai-repo.annailabs.com/ubuntu-12.04/genetorrent-common_3.8.5-ubuntu2-12.04_amd64.deb
+#wget --user=<FILLMEIN> --password=<FILLMEIN> http://annai-repo.annailabs.com/ubuntu-12.04/genetorrent-download_3.8.5-ubuntu2-12.04_amd64.deb
+
 
