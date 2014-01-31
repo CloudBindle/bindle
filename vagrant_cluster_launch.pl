1aa3dce3b29e7377523aeea73d3cea070170e70d
Brian O'Connor <briandoconnor@gmail.com>
2014-01-31 10:24:07 +0100

    Cleanup the box

--- vagrant_cluster_launch.pl
+++ vagrant_cluster_launch.pl
@@ -87,13 +87,13 @@ if (!defined($configs->{'SEQWARE_BUILD_CMD'})) { $configs->{'SEQWARE_BUILD_CMD'}
 
 # define the "boxes" used for each provider
 # TODO: these are hardcoded and may change
+# you can override for VirtualBox only via the json config
+# you can find boxes listed at http://www.vagrantbox.es/
 if ($launch_vb) {
   $launch_cmd = "vagrant up";
-
   # Allow a custom box to be specified
   if (!defined($configs->{'BOX'})) { $configs->{'BOX'} = "Ubuntu_12.04"; }
-  if (!defined($configs->{'BOX_URL'})) { $configs->{'BOX_URL'} = "http://cloud-images.ubuntu.com/vagrant/quantal/current/quantal-server-cloudimg-amd64-vagrant-disk1.box"; }
-  #####$configs->{'BOX_URL'} = "http://cloud-images.ubuntu.com/precise/current/precise-server-cloudimg-vagrant-amd64-disk1.box";
+  if (!defined($configs->{'BOX_URL'})) { $configs->{'BOX_URL'} = "http://cloud-images.ubuntu.com/precise/current/precise-server-cloudimg-vagrant-amd64-disk1.box"; }
 } elsif ($launch_os) {
   $launch_cmd = "vagrant up --provider=openstack";
   $configs->{'BOX'} = "dummy";
