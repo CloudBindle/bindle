e2a0c009b5299e2d928ed2ea72c503ff0129faf6
Ubuntu <ubuntu@brian-launcher.novalocal>
2014-03-20 04:57:45 +0000

    Match genetorrent package names in commands that download and install them

--- templates/server_setup_scripts/pancancer/ubuntu_12.04_pancancer_script.sh
+++ templates/server_setup_scripts/pancancer/ubuntu_12.04_pancancer_script.sh
@@ -10,9 +10,6 @@ apt-get -q -y --force-yes install liblz-dev zlib1g-dev libxml-dom-perl samtools 
 # dependencies for genetorrent 
 apt-get -q -y --force-yes install libboost-filesystem1.48.0 libboost-program-options1.48.0 libboost-regex1.48.0 libboost-system1.48.0 libicu48 libxerces-c3.1 libxqilla6
 rm genetorrent*
-#wget --user=oicr --password=oicr2013 http://annai-repo.annailabs.com/ubuntu-12.04/genetorrent-upload_3.8.5-ubuntu2-12.04_amd64.deb
-#wget --user=oicr --password=oicr2013 http://annai-repo.annailabs.com/ubuntu-12.04/genetorrent-common_3.8.5-ubuntu2-12.04_amd64.deb
-#wget --user=oicr --password=oicr2013 http://annai-repo.annailabs.com/ubuntu-12.04/genetorrent-download_3.8.5-ubuntu2-12.04_amd64.deb
 wget http://cghub.ucsc.edu/software/downloads/GeneTorrent/3.8.5/genetorrent-common_3.8.5-ubuntu2.91-12.04_amd64.deb
 wget http://cghub.ucsc.edu/software/downloads/GeneTorrent/3.8.5/genetorrent-download_3.8.5-ubuntu2.91-12.04_amd64.deb
 wget http://cghub.ucsc.edu/software/downloads/GeneTorrent/3.8.5/genetorrent-upload_3.8.5-ubuntu2.91-12.04_amd64.deb
@@ -21,5 +18,5 @@ wget http://cghub.ucsc.edu/software/downloads/GeneTorrent/3.8.5/genetorrent-uplo
 wget https://cghub.ucsc.edu/software/downloads/cghub_public.key
 
 # finally install these
-dpkg -i genetorrent-upload_3.8.5-ubuntu2-12.04_amd64.deb  genetorrent-common_3.8.5-ubuntu2-12.04_amd64.deb genetorrent-download_3.8.5-ubuntu2-12.04_amd64.deb
+dpkg -i genetorrent-common_3.8.5-ubuntu2.91-12.04_amd64.deb genetorrent-download_3.8.5-ubuntu2.91-12.04_amd64.deb genetorrent-upload_3.8.5-ubuntu2.91-12.04_amd64.deb
 
