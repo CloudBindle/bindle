178732bcccabb88cd9071a625dbf72fec4382cc6
Brian O'Connor <boconnor@olm-boconnorair.local>
2014-03-21 06:48:54 +0900

    Seems like the last line doesn't get executed unless you add a newline after it

--- templates/server_setup_scripts/seqware/ubuntu_12.04_seqware_master_install_script.sh
+++ templates/server_setup_scripts/seqware/ubuntu_12.04_seqware_master_install_script.sh
@@ -137,3 +137,4 @@ su - seqware -c '(echo "* * * * * ~seqware/crons/status.cron >> ~seqware/logs/st
 cd ~seqware
 wget https://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_HelloWorld_1.0-SNAPSHOT_SeqWare_1.0.11.zip
 su - seqware -c 'seqware bundle install --zip Workflow_Bundle_HelloWorld*'
+
