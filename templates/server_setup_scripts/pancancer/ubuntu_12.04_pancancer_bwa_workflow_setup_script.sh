486b6ebf58a6b2be7d8d3bbd0257348cb447f0ef
Brian O'Connor <boconnor@my-mac-pro.local>
2014-03-29 07:16:03 -0400

    Updating the template for installing workflow

--- templates/server_setup_scripts/pancancer/ubuntu_12.04_pancancer_bwa_workflow_setup_script.sh
+++ templates/server_setup_scripts/pancancer/ubuntu_12.04_pancancer_bwa_workflow_setup_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~seqware
-wget http://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_PanCancer_BWA_Mem_2.1-SNAPSHOT_SeqWare_1.0.11.zip
-chmown seqware:seqware Workflow_Bundle_PanCancer_BWA_Mem_2.1-SNAPSHOT_SeqWare_1.0.11.zip
-su - seqware -c 'seqware bundle install --zip Workflow_Bundle_PanCancer_BWA_Mem_2.1-SNAPSHOT_SeqWare_1.0.11.zip'
+wget http://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip
+chmown seqware:seqware Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip
+su - seqware -c 'seqware bundle install --zip Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip'
 
