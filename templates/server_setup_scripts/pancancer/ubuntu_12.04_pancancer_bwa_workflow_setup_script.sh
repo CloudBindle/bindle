467ee002f9d9bf7d22b9966b580f630444c488e6
Brian O'Connor <boconnor@my-mac-pro.local>
2014-03-29 09:30:07 -0400

    Fixed a typo, removed the zip after install

--- templates/server_setup_scripts/pancancer/ubuntu_12.04_pancancer_bwa_workflow_setup_script.sh
+++ templates/server_setup_scripts/pancancer/ubuntu_12.04_pancancer_bwa_workflow_setup_script.sh
@@ -2,6 +2,7 @@
 
 cd ~seqware
 wget http://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip
-chmown seqware:seqware Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip
+chown seqware:seqware Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip
 su - seqware -c 'seqware bundle install --zip Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip'
+rm Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip
 
