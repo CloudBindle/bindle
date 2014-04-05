c38493c373b0ee0fb0663b27f07ea408a7e0e71f
Brian O'Connor <boconnor@my-mac-pro.local>
2014-04-04 21:20:02 -0400

    Improving the pancancer scripts

--- templates/server_setup_scripts/pancancer/ubuntu_12.04_pancancer_bwa_workflow_setup_script.sh
+++ templates/server_setup_scripts/pancancer/ubuntu_12.04_pancancer_bwa_workflow_setup_script.sh
@@ -3,6 +3,6 @@
 cd ~seqware/released-bundles/
 wget http://s3.amazonaws.com/oicr.workflow.bundles/released-bundles/Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip
 chown seqware:seqware Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip
-su - seqware -c 'seqware bundle install --zip Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip'
+su - seqware -c 'seqware bundle install --zip ~seqware/released-bundles/Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip'
 rm Workflow_Bundle_BWA_2.1_SeqWare_1.0.11.zip
 
