dc75342c4b9da92d048b73659c21c98a99fd3c82
Brian O'Connor <briandoconnor@gmail.com>
2014-04-15 12:07:47 -0400

    Added a new hosts setup step, prototyping on the pancancer seqware cluster config

--- templates/server_setup_scripts/ubuntu_12.04_master_script.sh
+++ templates/server_setup_scripts/ubuntu_12.04_master_script.sh
@@ -1,18 +1,5 @@
 #!/bin/bash -vx
 
-## first, set the hostname
-#hostname master
-#
-## fix the /etc/hosts file since SGE wants reverse lookup to work
-#cp /etc/hosts /tmp/hosts
-#echo '127.0.0.1 localhost' > /etc/hosts
-#echo `/sbin/ifconfig  | grep -A 3 eth0 | grep 'inet addr' | perl -e 'while(<>){ chomp; /inet addr:(\d+\.\d+\.\d+\.\d+)/; print $1; }'` `hostname` >> /etc/hosts
-#cat /tmp/hosts | grep -v '127.0.1.1' | grep -v `hostname` | grep -v localhost | >> /etc/hosts
-#
-## setup hosts
-## NOTE: the hostname seems to already be set at least on BioNimubs OS
-#echo '%{HOSTS}' >> /etc/hosts
-
 # general apt-get
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
