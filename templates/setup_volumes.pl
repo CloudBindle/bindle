15d77863045d0f86f611f2bf8851ba9fd193b291
Brian O'Connor <briandoconnor@gmail.com>
2014-04-22 15:19:42 -0400

    Trying to turn encryption back on

--- templates/setup_volumes.pl
+++ templates/setup_volumes.pl
@@ -59,11 +59,9 @@ foreach my $dev (@list) {
   }
   my $mount_path = find_mount_path($dev);
   # if ecryptfs was success, the mount path gets encrypted added to it
-  # HACK
-  # turn off encryption for now
-  #if(setup_ecryptfs($mount_path)) {
-  #  $mount_path = $mount_path."/encrypted";
-  #}
+  if(setup_ecryptfs($mount_path)) {
+    $mount_path = $mount_path."/encrypted";
+  }
   # add to the list of mounted dirs
   $final_list .= "$mount_path\n";
 }
