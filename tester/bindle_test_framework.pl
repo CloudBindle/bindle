1757162d37f429deaae672d2aaa84dd199de1276
amishpatel1994 <amishpatel1994@github.com>
2014-08-15 18:26:09 +0000

    refactoring bindle_test_framework.pl

--- tester/bindle_test_framework.pl
+++ tester/bindle_test_framework.pl
@@ -15,22 +15,19 @@ use tests;
 use launch;
 use parser;
 use threads;
-#use Term::ProgressBar;
 use File::Spec;
 
 
-my $bindle_folder_path = "";
 my $config_paths = "";
 my $html_doc = HTML::Manipulator::Document->from_file('tester/template.html');
 my $help = 0;
 my $destroy_clusters = 0;
 my $exit_code = 0;
-GetOptions ("bindle-folder-path=s" => \$bindle_folder_path,
-            "use-config-paths=s" => \$config_paths,
+GetOptions ("use-config-paths=s" => \$config_paths,
             "destroy-clusters" => \$destroy_clusters,
             "help" => \$help);
 if ($help){
-    die "\n--------------------------------------------------------------------------------\nUSAGE: \n\t --use-config-paths <bindle_config file paths> Note: Pass in a list of bindle config paths relative to the bindle directory separated by comma(Ex. tester/bindle_configs/aws.cfg,tester/bindle_configs/vcloud.cfg\n--------------------------------------------------------------------------------\n";
+    die "\n--------------------------------------------------------------------------------\nUSAGE: \n\t --use-config-paths <bindle_config file paths> Note: Pass in a list of bindle config paths relative to the bindle directory separated by comma(Ex. tester/bindle_configs/aws.cfg,tester/bindle_configs/vcloud.cfg\n\t--destory-clusters Note: Using this ensures that the clusters will be destroyed after the testing is finished. You should still check the web console to make sure that the clusters do get deleted. \n\t --help\n--------------------------------------------------------------------------------\n";
 }
 
 my %cfg_path_files = ();
@@ -51,7 +48,8 @@ for my $config_path (@config_path_files){
     $cfg_path_files{"$config_path"} = $val;
 }
 
-print Dumper(%cfg_path_files);
+# if ~/.bindle/test_framework_configs/ doesn't exist, 
+# then tranfer all the .cfg files over from test/config_templates
 my $abs_path = `readlink -f ~/.bindle/`;
 $abs_path = (split(/\n/,$abs_path))[0];
 my $rel_path = File::Spec->abs2rel($abs_path,'.');
@@ -61,7 +59,9 @@ if (not -e "$rel_path/test_framework_configs"){
     die "Copied the test_framework configs to ~/.bindle/test_framework_configs. Please fill in the desired config files and try again!";
 }
 
+# removes known_hosts because this causes issues with Net::OpenSSH
 system("rm -r ~/.ssh/known_hosts");
+
 # goes through each environments and launches clusters and single node instances
 while (my ($key,$value) = each(%cfg_path_files)){
 
@@ -71,17 +71,20 @@ while (my ($key,$value) = each(%cfg_path_files)){
     # get the relative path of config file
     my $rel_path = parser->get_rel_path("~/.bindle/$value.cfg");    
 
-    # read in the cluster informations from the config file
+    # read in the cluster information from config file
     my $config_file = new Config::Simple("$rel_path");
+    
     # lauch the clusters
     my $test_results = launch_clusters($config_file,$key);    
     say "All the clusters have been launched and tested for $value! \nNow, generating an html document to show the test results!";
     if ($test_results =~ /FAIL/){
         $exit_code = 1;
     }
+    
     # record test results in html file
     $html_doc = parser->set_test_result($html_doc,$key,$test_results);
     say "Test results have been generated for $value!";
+    
     # destroy the clusters used for testing
     if ($destroy_clusters){
         say "Destroying all the clusters for $value!";
@@ -108,7 +111,6 @@ exit $exit_code;
 # SUBROUTINES
 
 sub launch_clusters{
-    # launch the clusters
     my ($cfg_file,$env_file) = @_;
     my $result = "";
     my $platform = $cfg_file->param('platform.type');
@@ -120,7 +122,7 @@ sub launch_clusters{
     my $environment = $cfg_file->param('platform.env');
     say "\tSTARTING TO LAUNCH ALL CLUSTERS FOR $environment";
     say "--------------------------------------------------------------------------------";
-    # launch all the clusters for a particular cloud environment (ex. aws)
+    # launch all the cluster blocks from a particular config file(ex. aws)
     $result .= launch_cluster_threads($number_of_clusters, $number_of_single_nodes, $platform,$cfg_file,$env_file,$result); 
     say "--------------------------------------------------------------------------------";
     say "\tLAUNCHED ALL CLUSTERS FOR $environment";
@@ -133,52 +135,49 @@ sub launch_clusters{
 sub launch_cluster_threads{
     my ($number_of_clusters,$number_of_nodes,$platform,$cfg_file,$env_file,$result) = @_;
     
-    #my $progress_count = 3*$number_of_clusters + 3*$number_of_nodes;
-    #my $progress = Term::ProgressBar->new ({count => $progress_count ,name => 'Progress'});
-    #my $update_progress = 0;
     my %threads;
     
+    # create threads for all the multi-node cluster blocks 
     for (my $i = 1; $i <= $number_of_clusters; $i += 1){
          say "STARTING A THREAD TO LAUNCH CLUSTERS FOR $env_file,cluster$i";
          my $cluster_name = "cluster$i";
-         my $thr = threads->create(\&launch_multi_node_cluster,$number_of_clusters,$platform,$cfg_file,$env_file,$result,$cluster_name);
+         my $thr = threads->create(\&launch_one_cluster,$number_of_clusters,$platform,$cfg_file,$env_file,$result,$cluster_name);
          $threads{"cluster$i"} = $thr;
-         #$update_progress += 1;
-         #$progress->update($update_progress);
          sleep 60;
     }
 
+    # create threads for all the single-node cluster blocks 
     for (my $i = 1; $i <= $number_of_nodes; $i += 1){
         my $node_name = "singlenode$i";
         say "STARTING A THREAD TO LAUNCH SINGLE NODE CLUSTERS FRO $env_file, singlenode$i";
-        my $thr = threads->create(\&launch_multi_node_cluster,$number_of_nodes,$platform,$cfg_file,$env_file,$result,$node_name);
+        my $thr = threads->create(\&launch_one_cluster,$number_of_nodes,$platform,$cfg_file,$env_file,$result,$node_name);
         $threads{"$node_name"} = $thr;
-        #$update_progress += 1;
-        #$progress->update($update_progress);
         sleep 60;
     }
     say " ALL LAUNCH THREADS STARTED";
     print Dumper(%threads);
 
+    # join all the threads to produce a result for a particular config file(aws.cfg)
     while (my ($key,$value) = each(%threads)){
         $result .= $value->join();
-        #$update_progress += 2;
-        #$progress->update($update_progress);
     }
 
-    #$progress->update($progress_count);
     say "ALL LAUNCH THREADS COMPLETED FOR $env_file";
     return $result;
 }
 
 
-sub launch_multi_node_cluster{
+sub launch_one_cluster{
         my ($number_of_clusters,$platform,$cfg_file,$env_file,$result,$cluster_name) = @_;
         my $working_dir = $cfg_file->param("$cluster_name.target_directory");
         my $seq_version = $cfg_file->param("platform.seqware_version");
         my $bwa_version = $cfg_file->param("platform.bwa_workflow_version");
         system("mkdir $working_dir");
+        
+        # launch the cluster via bindle
         system("perl bin/launcher/launch_cluster.pl --use-$platform --use-default-config --launch-cluster $cluster_name >> $working_dir/cluster.log");
+        
+        # ssh into the master node and test if bindle was provisioned properly
         my $float_ip = parser->get_float_ip($cfg_file->param("$cluster_name.target_directory"),"master");
         say "FLOATIP: $float_ip";
         my $ssh = launch->connect_to_host($float_ip,$cfg_file->param('platform.ssh_key_name'));
@@ -190,12 +189,14 @@ sub launch_multi_node_cluster{
 	else{
 	    $result .= tests->test_single_nodes_as_ubuntu($ssh,$working_dir,$seq_version,$bwa_version);
         }
+
         # record the result in the matrix
         $html_doc = parser->update_matrix($html_doc,$json_file,$env_file,$result);
         say "\n--------------------------------------------------------------------------------";
         say "\tLaunched cluster: \n\tPLATFORM = $platform\n\t CLUSTER BLOCK = $cluster_name";
         say "--------------------------------------------------------------------------------";
         say "RESULT: $result";
+
         return $result;
 }
 
