7aa86f507ccc84e6bb311bae145887c39709cbcb
Adam Wright <adam.wright@oicr.on.ca>
2014-10-29 17:00:22 +0000

    added changes for using config files and not json files

--- lib/cluster/config.pm
+++ lib/cluster/config.pm
@@ -4,157 +4,53 @@ use Config::Simple;
 use common::sense;
 use Carp::Always;
 use autodie qw(:all);
-use JSON;
 use Config;
 use Storable 'dclone';
 use Data::Dumper;
 use File::Spec;
 use Cwd 'abs_path';
-#reads in all the data written to the appropriate config file located at config/
-#and constructs an object in the form of configs and default_configs to match
-#the format the original script expects it to be in
-sub read_default_configs {
-  my ($class, $cluster_name, $launch_vcloud, $launch_aws, $launch_os, $launch_vb) = @_;
-  my $default_configs = new Config::Simple();
-  my $abs_path = "";
-  # gets the absolute path (ex. /home/ubuntu/.bindle/<platform>.cfg)
-  if ($launch_aws){
-      $abs_path = `readlink -f ~/.bindle/aws.cfg`;
-  }
-  elsif ($launch_os){ 
-      $abs_path = `readlink -f ~/.bindle/os.cfg`;
-  }
-  elsif ($launch_vcloud){
-      $abs_path = `readlink -f ~/.bindle/vcloud.cfg`;
-  }
-  elsif ($launch_vb) {
-    $abs_path = `readlink -f ~/.bindle/vb.cfg`;
-  }
-  $abs_path = (split(/\n/,$abs_path))[0];
-  my $rel_path = File::Spec->abs2rel($abs_path,'.');  
-  
-  # upgrade or copy the configs over to ~/.bindle/ if it doesn't exist
-  if (-e $abs_path and not upgrade_outdated_configs($abs_path,$rel_path)){
-    $default_configs->read($rel_path) or die $default_configs->error();
-  }else{
-    copy_over_config_templates();
-  }
-  
-  my $config_file = $default_configs->param("$cluster_name.json_template_file_path");
-  my $work_dir = make_target_directory($cluster_name,$default_configs);
-  my $temp_configs = read_json_config($config_file);
-  my $general_config = extract_general_config($temp_configs->{general},$default_configs,$launch_vcloud);
-  my ($temp_cluster_configs, $cluster_configs) = {};
-  
-  if ($launch_aws || $launch_os || $launch_vcloud){
-    $temp_cluster_configs = extract_node_config($temp_configs->{node_config},$default_configs,$launch_os, $cluster_name);
-  }
-  elsif ($launch_vb){ 
-    $temp_cluster_configs = $temp_configs->{node_config}; 
-    $general_config->{'BOX'} //= "Ubuntu_12.04"; 
-    $general_config->{'BOX_URL'} //= "http://cloud-images.ubuntu.com/precise/current/precise-server-cloudimg-vagrant-amd64-disk1.box";
-  }
-  else{ 
-    die "Don't understand the launcher type to use: AWS, OpenStack, VirtualBox, or vCloud. Please specify with a --use-* param"; 
-  }
 
-  foreach my $node_config (@{$temp_cluster_configs}){
-    my @names = @{$node_config->{'name'}};
-    for (0 .. $#names){
-      my $node_config_copy = dclone $node_config;
-      delete $node_config_copy->{'floatip'};
-      $node_config_copy->{'floatip'} = @{$node_config->{'floatip'}}[$_];
-      $cluster_configs->{$names[$_]} = $node_config_copy;
-    }
-  }
+sub read_config {
+    my ($class, $platform, $cluster_name) = @_;
+
+    my $config = new Config::Simple();
+
+    my $config_file = $ENV{"HOME"}."/.bindle/$platform.cfg";
+
+    copy_over_config_templates() unless (-e $config_file );
   
-  return($general_config, $cluster_configs, $work_dir,$config_file);
-}
+    $config->read($config_file) or die $config->error();
 
-#reads in the json file and gets rid of all the commented lines
-sub read_json_config {
-  my ($config_file) = @_;
-  open IN, '<', $config_file or die "No template JSON file detected in this directory!";
-  my $json_txt = "";
-
-  while(<IN>) {
-    next if (/^\s*#/);
-    $json_txt .= $_;
-  }
-  close IN;
-
-  my $temp_configs = decode_json($json_txt);
-  return $temp_configs;
-}
-
-#this extracts all the information needed to set up the particular cluster
-#that is, it extracts floating_ips and the number of nodes it will have
-sub extract_node_config {
-  my ($temp_cluster_configs, $default_configs,$launch_os, $cluster_name) = @_;
-
-  my (@worker_nodes, @float_ips, @os_float_ips) = ();
-  my $number_of_nodes = $default_configs->param("$cluster_name.number_of_nodes");
-  
-  if ($launch_os){
-    @os_float_ips = $default_configs->param("$cluster_name.floating_ips");
-    my @master_float_ip = $os_float_ips[0];
-    $temp_cluster_configs->[0]->{floatip} = \@master_float_ip;
-  }
-  
-  for (my $i = 1; $i < $number_of_nodes; $i++){
-    push(@worker_nodes,'worker'.$i);
-    if ($launch_os){
-      push(@float_ips, $os_float_ips[$i]);
-    }
-    else{
-      push(@float_ips, '<FILLMEIN>');
-    }
-  }
-  
-  $temp_cluster_configs->[1]->{name} = \@worker_nodes;
-  $temp_cluster_configs->[1]->{floatip} = \@float_ips;
-  return $temp_cluster_configs;
+    return $config;
 }
 
 #extracts all the platform related information from the config file
 sub extract_general_config {
-  my ($general_config, $default_configs,$launch_vcloud) = @_;
-  my $selected_platform = uc $default_configs->param('platform.type');
+    my ($general_config, $config, $platform) = @_;
+
+    my $selected_platform = uc $config->param('platform.type');
   
-  foreach my $key (sort keys $default_configs->param(-block=>'platform')) {
-    # define the "boxes" used for each provider
+    foreach my $key (sort keys $config->param(-block=>'platform')) {
+      # define the "boxes" used for each provider
     # These may be changed in the config file
     # you can override for VirtualBox only via the json config
     # you can find boxes listed at http://www.vagrantbox.es/
     if($key =~ /box|gluster/){
-      $general_config->{uc $key} = $default_configs->param('platform.'.$key);
+      $general_config->{uc $key} = $config->param('platform.'.$key);
     }
     else{
-      $general_config->{$selected_platform.'_'.(uc $key)} = $default_configs->param('platform.'.$key);
+      $general_config->{$selected_platform.'_'.(uc $key)} = $config->param('platform.'.$key);
     }
   }
   
-  my $pem_file = $default_configs->param('platform.ssh_key_name');
-  if ($launch_vcloud){
-      $general_config->{'VCLOUD_USER_NAME'} = $default_configs->param('platform.ssh_username');
-  }
-
+  my $pem_file = $config->param('platform.ssh_key_name');
+  $general_config->{'VCLOUD_USER_NAME'} = $config->param('platform.ssh_username')
+            if ($platform eq 'vcloud');          
   $general_config->{$selected_platform.'_SSH_PEM_FILE'} = "~/.ssh/".$pem_file.".pem";
   $general_config->{'SSH_PRIVATE_KEY_PATH'} = "~/.ssh/".$pem_file.".pem";
-  
 
-  
   return $general_config;
-}
-
-#reads in the target_directory specified in the config file and makes a folder
-sub make_target_directory {
-   my ($cluster_name,$default_configs) = @_;
-   
-   my $work_dir = $default_configs->param("$cluster_name.target_directory");
-   run($work_dir,"mkdir -p $work_dir");
-   
-   return $work_dir;
+  
 }
 
 sub run {
@@ -179,22 +75,6 @@ sub run {
     else {
         system($final_cmd);
     }
-}
-
-#adds on to the launch_command for vagrant according to the specified platform
-sub set_launch_command {
-  my ($class, $launch_aws, $launch_os, $launch_vcloud) = @_;
-  my $launch_command;
-  if ($launch_aws){
-    $launch_command = ' --provider=aws';
-  }
-  elsif ($launch_os){ 
-    $launch_command = ' --provider=openstack';
-  }
-  elsif ($launch_vcloud){
-    $launch_command = ' --provider=vcloud';
-  }
-  return $launch_command;
 }
 
 # copies the configs over to ~/.bindle/ and notifies the user to fill in the required info
--- lib/cluster/setup.pm
+++ lib/cluster/setup.pm
@@ -3,87 +3,173 @@ package cluster::setup;
 use IPC::System::Simple;
 use common::sense;
 use autodie qw(:all);
+use Data::Dumper;
 use Getopt::Long;
 use Config;
 $Config{useithreads} or die('Recompile Perl with threads to run this program.');
 use threads;
 use Storable 'dclone';
 use Carp::Always;
-
+use Config::Simple;
 my $configs;
 my $work_dir;
 
-
-# this assumes the first pass setup script was created per host by setup_os_config_scripts
-# FIXME: should remove the non-generic files processed below if possible, notice how there are project-specific file copies below!
 sub prepare_files {
-    my ($class, $cluster_configs, $configs, $work_dir, $vb_ram, $vb_cores, @ebs_vols) = @_;
-    # Vagrantfile, the core file used by Vagrant that defines each of our nodes
-    $configs = setup_vagrantfile("templates/Vagrantfile_start.template", 
-                      "templates/Vagrantfile_part.template", 
-                      "templates/Vagrantfile_end.template", 
-                      $cluster_configs, $configs, "$work_dir", $vb_ram, $vb_cores, @ebs_vols);
+    my ($class, $platform, $nodes, $config, $work_dir) = @_;
 
-    foreach my $node (sort keys %{$cluster_configs}) {
-        # settings, user data
+    add_vagrantfiles($config, $platform, $work_dir, $nodes);
+    add_user_data_files($nodes, $work_dir);
+
+    return;
+}
+
+sub add_user_data_files {
+    my ($nodes, $work_dir) = @_;
+
+    foreach my $node (@{$nodes}) {
         copy("templates/user_data.txt", "$work_dir/$node/user_data.txt");
     }
+}
+
+sub add_vagrantfiles {
+   my ($config, $platform, $work_dir, $nodes) = @_;
+
+   foreach my $node (@{$nodes}) {
+        add_vagrantfile($config, $platform, $work_dir, $node);
+   }
+}
+
+sub add_vagrantfile {
+    my ($config, $platform, $work_dir, $node) = @_;
+
+    my $vagrantfile_template_map = vagrantfile_template_map($platform, $node, $config);
+
+    my $vagrantfile = "$work_dir/$node/Vagrantfile";
+    say $vagrantfile;
+    autoreplace('templates/Vagrantfile.template', $vagrantfile, $vagrantfile_template_map);
+    # hack to deal with empty network/floatIP
+    my $full_output = `cat $vagrantfile`;
+    $full_output =~ s/os.network = "<FILLMEIN>"//;
+    $full_output =~ s/os.network = ""//;
+	$full_output =~ s/os.networks = \[ "<FILLMEIN>" \]/os.networks = \[ \]/;
+	$full_output =~ s/os.networks = \[ "" \]/os.networks = \[ \]/;
+    $full_output =~ s/os.floating_ip = "<FILLMEIN>"//;
+    $full_output =~ s/os.floating_ip = ""//;
+
+    open my $vout, '>', $vagrantfile;
+    print $vout $full_output;
+    close $vout;
+
     return $configs;
 }
 
-sub setup_vagrantfile {
-    my ($start, $part, $end, $cluster_configs, $configs, $work_dir, $ram, $cores, @ebs_vols) = @_;
-    foreach my $node (sort keys %{$cluster_configs}) {
-        $configs->{custom_hostname} = $node;
-        $configs->{VB_CORES} = $cores;
-        $configs->{VB_RAM} = $ram;
-        $configs->{OS_FLOATING_IP} = $cluster_configs->{$node}{floatip};
-        if (not exists $configs->{AWS_REGION}){
-    	    $configs->{AWS_REGION} = "us-east-1";
+sub vagrantfile_template_map {
+    my ($platform, $node, $config) = @_;
+
+    my %vagrantfile_map = (
+       'BOX_URL' => $config->param('platform.box_url'),
+       'BOX'     => $config->param('platform.box'),
+       'custom_hostname' => $node,
+    );
+
+    if ($platform eq 'virtualbox') {
+        if ($config->param('platform.vb_ram') ) {
+           $vagrantfile_map{VB_RAM} = $config->param('plaform.vb_ram');
         }
-        if (not exists $configs->{AWS_ZONE} or $configs->{AWS_ZONE} eq "nil" ){
-    	    $configs->{AWS_ZONE} = "nil";
+        else {
+            die 'Specify the parameter vb_ram in the platform block of the config file';
         }
-        elsif ($configs->{AWS_ZONE} !~ /^"\S+"$/) { 
-            $configs->{AWS_ZONE} = "\"$configs->{AWS_ZONE}\"";
+        if ($config->param('platform.hostname') ) {
+            $vagrantfile_map{VB_CORES} = $config->param('plaform.vb_cores');
+        }
+        else {
+            die 'Specify the parameter vb_cores in the platform block of the config file';
         }
 
-        $configs->{AWS_EBS_VOLS} = "";
-        if (scalar @ebs_vols > 0){
-            $configs->{AWS_EBS_VOLS} .= "aws.block_device_mapping = [";
-            # starts at "f=102"
-            my $count = 102;
-            foreach my $size (@ebs_vols){
-                my $current_name = chr $count;
-    	        $configs->{AWS_EBS_VOLS} .= "{'DeviceName' => \"/dev/sd$current_name\", 'VirtualName' => \"block_storage\", 'Ebs.VolumeSize' => $size, 'Ebs.DeleteOnTermination' => true},";
+        my @ebs_vols = $config->param('plaform_vols');
+        if (scalar @ebs_vols > 0) {
+                $vagrantfile_map{AWS_EBS_VOLS} .= "aws.block_device_mapping = [";
+                # starts at "f=102"
+                my $count = 102;
+                foreach my $size (@ebs_vols){
+                    my $current_name = chr $count;
+    	            $vagrantfile_map{AWS_EBS_VOLS} .= "{'DeviceName' => \"/dev/sd$current_name\", 'VirtualName' => \"block_storage\", 'Ebs.VolumeSize' => $size, 'Ebs.DeleteOnTermination' => true},";
     	        $count++;
     	    }
-            chop $configs->{AWS_EBS_VOLS};
-    	    $configs->{AWS_EBS_VOLS} .= "]";
+            chop $vagrantfile_map{AWS_EBS_VOLS};
+    	    $vagrantfile_map{AWS_EBS_VOLS} .= "]";
         }
-        my $node_output = "$work_dir/$node/Vagrantfile";
-	print "$node_output\n";
-        autoreplace("$start", "$node_output", $configs);
-        # FIXME: should change this var to something better
-        autoreplace("$part", "$node_output.temp", $configs);
-        run("cat $node_output.temp >> $node_output");
-        run("rm $node_output.temp");
-        run("cat $end >> $node_output");
-        # hack to deal with empty network/floatIP
-        my $full_output = `cat $node_output`;
-        # HACK: this is a hack because we don't properly templatize the Vagrantfile... I'm doing this to eliminate empty os.network and os.floating_ip which cause problems on various OpenStack clouds
-        $full_output =~ s/os.network = "<FILLMEIN>"//;
-        $full_output =~ s/os.network = ""//;
-	$full_output =~ s/os.networks = \[ "<FILLMEIN>" \]/os.networks = \[ \]/;
-	$full_output =~ s/os.networks = \[ "" \]/os.networks = \[ \]/;
-        $full_output =~ s/os.floating_ip = "<FILLMEIN>"//;
-        $full_output =~ s/os.floating_ip = ""//;
-        open my $vout, '>', $node_output;
-        print $vout $full_output;
-        close $vout;
+        else {
+            die 'Specify the parameter ebs_vols in the platform block of the config file';
+        }
     }
-    
-    return $configs;
+    elsif ($platform eq 'vcloud') {
+        if (my $hostname = $config->param('platform.hostname') ) {
+            $vagrantfile_map{VCLOUD_HOSTNAME} = $hostname;
+        }
+        else {
+            die 'Specify the parameter hostname in the platform block of the config file';
+        }
+        if (my $username = $config->param('platform.username') ) {
+            $vagrantfile_map{VCLOUD_USERNAME} = $username;
+        }
+        else { 
+            die 'Specify the parameter username in the platform block of the config file';
+        }
+        if (my $password = $config->param('platform.password')) {
+           $vagrantfile_map{VCLOUD_PASSWORD} = $password;
+        }
+        else {
+            die 'Specify the parameter password in the platform block of the config file';
+        }
+        if (my $org_name = $config->param('platform.org_name') ) {
+           $vagrantfile_map{VCLOUD_ORG_NAME} = $org_name;
+        }
+        else {
+            die 'Specify the parameter org_name in the platform block of the config file';
+        }
+        if (my $vdc_name = $config->param('platform.vdc_name') ) {
+           $vagrantfile_map{VCLOUD_VDC_NAME} = $vdc_name;
+        }
+        else {
+            die 'Specify the parameter vdc_name in the platform block of the config file';
+        }
+       if (my $catalog_name = $config->param('platform.catalog_name') ) {
+           $vagrantfile_map{VCLOUD_CATALOG_NAME} = $catalog_name;
+        }
+        else {
+            die 'Specify the parameter catalog in the platform block of the config file';
+        }
+        if (my $vdc_network_name = $config->param('platform.vdc_network_name') ) {
+        $vagrantfile_map{VCLOUD_VDC_NETWORK_NAME} = $vdc_network_name;
+        }
+        else {
+            die 'Specify the parameter vdc_network_name in the platform block of the config file';
+        }
+        if (my $ssh_key_name = $config->param('platform.ssh_key_name') ) {
+            $vagrantfile_map{VCLOUD_SSH_PEM_FILE} = "~/.ssh/$ssh_key_name.pem";
+        }
+        else {
+            die 'Specify the parameter ssh_key_name in the platform block of the config file';
+        }
+
+        
+    }
+    elsif( $platform eq 'aws') {
+        if (my $key = $config->param('platform.key') ) {
+            $vagrantfile_map{AWS_KEY} = $key;
+        }
+        else {
+            die 'Specify the parameter key (AWS_KEY) in the platform block of the config file';
+        }
+    }
+
+=head
+        $configs->{OS_FLOATING_IP} = $cluster_configs->{$node}{floatip};
+
+=cut
+ 
+    return \%vagrantfile_map;
 }
 
 sub autoreplace {
